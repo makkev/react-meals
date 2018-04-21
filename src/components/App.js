22e8a776a92fe3c6b2da77efe4a1ee7e5d5587fd
K Mak <kumarajiva08@gmail.com>
2018-04-22 08:41:37 +1000

    Use connect()

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log('Props', this.props);
     return (
       <div>
         Hello World
@@ -10,4 +12,24 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  console.log('calendar ==>', calendar['sunday']);
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      // TODO: find out if it is possible to use the commented out code
+      // meals: calendar[day],
+      // meals: Object.assign({}, calendar[day])
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+        return meals
+      }, {}) 
+    })),
+  }
+}
+
+export default connect(mapStateToProps)(App);
+// export default App;
